cd690fd58d1efb3d512e96fd58a1af763ee84316
lijie <1620019745@qq.com>
2018-03-06 20:54:46 +0800

    add led.c

--- HARDWARE/LED/led.c
+++ HARDWARE/LED/led.c
@@ -17,7 +17,7 @@
 //LED IO初始化
 void LED_Init(void)
 {
- 
+ int a;
  GPIO_InitTypeDef  GPIO_InitStructure;
  	
  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB|RCC_APB2Periph_GPIOE, ENABLE);	 //使能PB,PE端口时钟
--- USER/main.c
+++ USER/main.c
@@ -21,8 +21,8 @@
 	LED_Init();		  	//初始化与LED连接的硬件接口
 	while(1)
 	{
-		LED0=0;
-		LED1=1;
+//		LED0=0;
+//		LED1=1;
 		delay_ms(300);	 //延时300ms
 		LED0=1;
 		LED1=0;
